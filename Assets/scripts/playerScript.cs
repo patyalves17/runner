0aec164fa25a84ce2e4e701af4e41e69925d11e9
logonrm <logonrm@fiap.com.br>
2018-02-17 17:25:12 -0200

    zombie

---
+++ Assets/scripts/playerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour {
+
+	public float velocidade;
+	float direcao_x;
+	float posicaoinicial_x, posicaoFinal_x;
+	public Transform[] posicoes;
+	int posicaoAtual=1;
+
+	// Update is called once per frame
+	void Update () {
+		//Fire1 - clique do mouse, touch ou ctrl esquerdo
+		if(Input.GetButtonDown("Fire1")){
+			//posicao do primeiro toque na tela
+			posicaoinicial_x = Input.mousePosition.x;
+		}else if(Input.GetButtonUp("Fire1")){
+			//posicao da ultima posicao do dedo na tela
+			posicaoFinal_x = Input.mousePosition.x;
+			//define a direcao
+			direcao_x = posicaoFinal_x - posicaoinicial_x;
+			if (direcao_x > 0 && posicaoAtual < 2) {
+				posicaoAtual++;
+			} else if(direcao_x <0 && posicaoAtual > 0){
+				posicaoAtual--;
+			}
+		}
+
+		//desloca o cubo para pista atual
+		transform.position = Vector3.Lerp(transform.position, 
+								posicoes[posicaoAtual].position, 
+								velocidade*Time.deltaTime);
+
+		print (posicaoAtual);
+	}
+}
